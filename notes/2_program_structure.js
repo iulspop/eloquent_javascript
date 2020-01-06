1c6af57138dd8d136443b54135db9637950e0e9f
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-01-05 20:47:27 -0500

    more while loop and do loop

--- notes/2_program_structure.js
+++ notes/2_program_structure.js
@@ -1,19 +1,39 @@
-// #Control Flow
+// #	Control Flow
 
-// ## While Loop
-// while expression truthy, loop block
+// ##	While Loop
+// // while expression truthy, loop block
 
-let num = 0;
-let count = 0;
+// let num = 0;
+// let count = 0;
 
-while (num < 10) {
-	alphabet = ["a", "b", "c", "d", "e", "f", "g"];
-	console.log(alphabet[count])
+// while (num < 10) {
+// 	alphabet = ["a", "b", "c", "d", "e", "f", "g"];
+// 	console.log(alphabet[count])
 
-	if (count == 6) {
-		count = 0;
-	} else {
-		count += 1;
-	}
-	num += 1;
-}
+// 	if (count == 6) {
+// 		count = 0;
+// 	} else {
+// 		count += 1;
+// 	}
+// 	num += 1;
+// }
+
+// let nthPower = 2;
+// let sum = 1; //  keeps track of
+// let loopCount = 0;
+
+// while (loopCount < nthPower) {
+// 	sum *= 2;
+// 	loopCount += 1;
+// }
+
+// console.log(sum);
+
+
+// // Do loop always executes it's body at least once
+// let pop = 1;
+// do {
+// 	pop *= 2;
+// } while (pop < 100);
+
+// console.log(pop);
