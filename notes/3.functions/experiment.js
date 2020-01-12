50e09e99eea7b5c0de00424f840d7a27c4e92cf7
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-01-12 11:14:09 -0500

    add 3.functions experiments

---
+++ notes/3.functions/experiment.js
@@ -0,0 +1,19 @@
+// @ts-check
+// let i = 0;
+
+// setInterval(() => {
+// 	console.log('iteration' + i)
+// 	i++;
+// }, 1000)
+
+const factorial = function (x) {
+	let x =
+	let sum = 0;
+	while (x > 2) {
+		sum = (sum || x) * (x - 1);
+		x--;
+	}
+	console.log(sum);
+}
+
+factorial(5);
