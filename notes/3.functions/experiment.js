5d340fb361d464ac6985318c7974cf945597fb79
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-01-13 17:56:07 -0500

    Test function hoisting

--- notes/3.functions/experiment.js
+++ notes/3.functions/experiment.js
@@ -6,14 +6,35 @@
 // 	i++;
 // }, 1000)
 
-const factorial = function (x) {
-	x = 5;
-	let sum = 0;
-	while (x > 2) {
-		sum = (sum || x) * (x - 1);
-		x--;
-	}
-	console.log(sum);
+// const factorial = function (x) {
+// 	x = 5;
+// 	let sum = 0;
+// 	while (x > 2) {
+// 		sum = (sum || x) * (x - 1);
+// 		x--;
+// 	}
+// 	console.log(sum);
+// }
+// factorial(10);
+
+// let x = 0;
+// function nestBlocks() {
+// 	let x = 5;
+// 	{
+// 		var y = 5;
+// 		x = x * x;
+// 	}
+// 	x = x * y;
+// 	return x;
+// }
+// x = 100;
+// nestBlocks()
+
+// Testing Function Hoisting
+function timeMachine() {
+	return "a great flood";
 }
 
-factorial(2);
+{
+	console.log("The weather 10000 years ago was:", timeMachine());
+}
