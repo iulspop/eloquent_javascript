3d1a1108d77098afc5cb8d55cc8e06f3c0f8d235
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-04-23 13:04:51 -0400

    create listToArray function

--- 4.data_structure/list-functions.js
+++ 4.data_structure/list-functions.js
@@ -9,4 +9,22 @@ const arrayToList = (array) => {
   return list;
 };
 
-module.exports = { arrayToList };
+const listToArray = (list) => {
+  let array = [];
+
+  if (list.value) {
+    array.push(list.value);
+    checkRest(list.rest);
+  }
+
+  function checkRest(rest) {
+    if (rest) {
+      array.push(rest.value);
+      checkRest(rest.rest);
+    }
+  }
+
+  return array;
+};
+
+module.exports = { arrayToList, listToArray };
--- 4.data_structure/list-functions.test.js
+++ 4.data_structure/list-functions.test.js
@@ -1,10 +1,30 @@
-const { arrayToList } = require("./list-functions");
+const { arrayToList, listToArray } = require("./list-functions");
 
 describe("arrayToList", () => {
-  it("returns list (nested set of objects) from array argument", () => {
+  it("returns list (nested set of objects) from array", () => {
     expect(arrayToList([10, 20])).toEqual({ value: 10, rest: { value: 20, rest: null } });
   });
-  it("returns list (nested set of objects) from array argument", () => {
+  it("returns list (nested set of objects) from array", () => {
     expect(arrayToList([10, 20, 30])).toEqual({ value: 10, rest: { value: 20, rest: { value: 30, rest: null } } });
+  });
+});
+
+describe("listToArray", () => {
+  it("returns array from list", () => {
+    expect(listToArray({ value: 10, rest: { value: 20, rest: null } })).toEqual([10, 20]);
+  });
+  it("returns array empty array from empty list", () => {
+    expect(listToArray({})).toEqual([]);
+  });
+  it("returns array from list", () => {
+    expect(listToArray({ value: 10, rest: { value: 20, rest: { value: 30, rest: null } } })).toEqual([10, 20, 30]);
+  });
+  it("returns array from list", () => {
+    expect(
+      listToArray({
+        value: 10,
+        rest: { value: 55, rest: { value: 30, rest: { value: 40, rest: { value: 50, rest: null } } } },
+      })
+    ).toEqual([10, 55, 30, 40, 50]);
   });
 });
