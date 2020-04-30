cc66c1e26a3c95a0ddea769cecc6ab24431f2b2b
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-04-30 11:13:58 -0400

    play with array.map

--- 5.higher_order_functions/playground.js
+++ 5.higher_order_functions/playground.js
@@ -5,3 +5,12 @@
 
   console.log(array.filter(el => el > 3))
 }
+
+
+{
+  console.log("play with array.map(transform)")
+
+  let array = [0, 1, 2, 3, 4, 5]
+
+  console.log(array.map(el => el + 3))
+}
