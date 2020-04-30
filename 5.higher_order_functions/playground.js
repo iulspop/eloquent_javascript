b8eba91627032000108af3add58e105ac984a859
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2020-04-30 13:25:50 -0400

    play with array.reduce

--- 5.higher_order_functions/playground.js
+++ 5.higher_order_functions/playground.js
@@ -14,3 +14,20 @@
 
   console.log(array.map(el => el + 3))
 }
+
+{
+  console.log("create reduce(array, combine, start)")
+
+  let array = [0, 1, 2, 3, 4, 5]
+
+  const fold = (array, combine, start = array[0]) => {
+    let current = start
+    for (let element of array) {
+      current = combine(current, element)
+    }
+    return current
+  }
+
+  console.log(fold(array, (a, b) => a + b))
+  console.log(array.reduce((a, b) => a === 0 ? a + b : a * b))
+}
